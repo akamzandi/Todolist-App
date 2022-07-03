e34d582eec608fe9e5ec8a1a0bba07df967cc03e
Akam Zandi <zandi.dev@gmail.com>
2022-07-03 21:31:26 +0430

    updated styles for a completed todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,7 @@
 const Todo = ({ todo, onEdit, onComplete }) => {
   return (
     <div className={"todo"}>
-      <div>{todo.text}</div>
+      <div className={todo.isCompleted ? "completed" : ""}>{todo.text}</div>
       <div>
         <button onClick={onEdit}>Edit</button>
         <button onClick={onComplete}>Completed</button>
