5e77d8dc02fbafd21cc6e29c73250c839a38da61
Akam Zandi <zandi.dev@gmail.com>
2022-07-03 11:39:46 +0430

    moving render to renderTodos func for readability

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,22 @@
 const TodoList = ({ todos }) => {
-  if (todos.length === 0) {
-    return <p>add some todos!</p>;
-  }
-
-  return (
-    <div>
-      {todos.map((todo) => {
-        return (
-          <div key={todo.id}>
-            <div>{todo.text}</div>
-            <div>
-              <button>Edit</button>
-              <button>Completed</button>
-            </div>
+  const renderTodos = () => {
+    if (todos.length === 0) {
+      return <p>add some todos!</p>;
+    }
+    return todos.map((todo) => {
+      return (
+        <div key={todo.id}>
+          <div>{todo.text}</div>
+          <div>
+            <button>Edit</button>
+            <button>Completed</button>
           </div>
-        );
-      })}
-    </div>
-  );
+        </div>
+      );
+    });
+  };
+
+  return <div>{renderTodos()}</div>;
 };
 
 export default TodoList;
