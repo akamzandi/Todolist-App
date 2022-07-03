dacbefe07613afefff0f723495c4886b45be109c
Akam Zandi <zandi.dev@gmail.com>
2022-07-03 10:14:44 +0430

    TodoFrom component created and initialized

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,5 +1,11 @@
+import TodoFrom from "./TodoForm";
+
 const TodoApp = () => {
-  return <div className="container">todos</div>;
+  return (
+    <div className="container">
+      <TodoFrom />
+    </div>
+  );
 };
 
 export default TodoApp;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,10 @@
+const TodoFrom = () => {
+  return (
+    <form>
+      <input />
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default TodoFrom;
